feab547513059b5bcc64203d1fd2a809620ab8b0
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-07-05 17:06:47 +0200

    added portfolio

--- src/App.js
+++ src/App.js
@@ -2,56 +2,55 @@ import './App.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
   return (
-  <>
-    <Navbar collapseOnSelect expand="lg" className="navbar-color">
-      <Container className="">
-      <img
-          src="CCminit.png"
-          height="70"
-          className="d-inline-block align-top charles-logo"
-          alt="React Bootstrap logo"
-          href="/accueil"
-      />
-
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="media-collapse" data-bs-theme="dark" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto mx-auto typo">
-            <Nav.Link  href="/accueil">Accueil</Nav.Link>
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/tarifs">Tarifs</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-          <div className="social-position">
-            <a href='https://fr-fr.facebook.com/'>
+    <>
+      <Navbar collapseOnSelect expand="lg" className="navbar-color">
+        <Container className="">
+          <Link to="/accueil">
             <img
-          src="Facebook_logo.png"
-          height="45"
-          className="d-inline-block align-top fb-logo"
-          alt="React Bootstrap logo"
-      />
-            </a>
-        <a href='https://www.instagram.com'>
-        <img
-          src="Instagram_logo.webp"
-          height="45"
-          className="d-inline-block align-top insta-logo"
-          alt="React Bootstrap logo"
-      />
-        </a>
+              src="CCminit.png"
+              height="70"
+              className="d-inline-block align-top charles-logo"
+              alt="React Bootstrap logo"
+            />
+          </Link>
 
-        </div>
-        </Navbar.Collapse>
-
-      </Container>
-    </Navbar>
-    <Outlet/>
-  </>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" className="media-collapse" data-bs-theme="dark" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto mx-auto typo">
+              <Link to="/accueil" className="nav-link">Accueil</Link>
+              <Link to="/portfolio" className="nav-link">Portfolio</Link>
+              <Link to="/tarifs" className="nav-link">Tarifs</Link>
+              <Link to="/contact" className="nav-link">Contact</Link>
+            </Nav>
+            <div className="social-position">
+              <a href='https://fr-fr.facebook.com/'>
+                <img
+                  src="Facebook_logo.png"
+                  height="45"
+                  className="d-inline-block align-top fb-logo"
+                  alt="React Bootstrap logo"
+                />
+              </a>
+              <a href='https://www.instagram.com'>
+                <img
+                  src="Instagram_logo.webp"
+                  height="45"
+                  className="d-inline-block align-top insta-logo"
+                  alt="React Bootstrap logo"
+                />
+              </a>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <Outlet />
+      
+    </>
   );
 }
 
--- src/Routes/Portfolio.js
+++ src/Routes/Portfolio.js
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from 'react';
+import './portfolio.css'
+import Footer from './Footer.js'
+
 
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <>
+        <h1 className='title-page'>PORTFOLIO</h1>
+    <hr></hr>
+          <div class="container overflow-hidden py-6">
+        <div class="grid-wrapper col">
+            <div class="lc-block">
+                <img class="img-fluid rounded rounded" src="https://images.unsplash.com/photo-1541845157-a6d2d100c931?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588282322673-c31965a75c3e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1351&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="tall lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588117472013-59bb13edafec?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" alt="" loading="lazy"></img>
+            </div>
+            <div class="wide lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1587588354456-ae376af71a25?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="lc-block">
+                <img class="img-fluid rounded" src=" https://images.unsplash.com/photo-1558980663-3685c1d673c4?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=60" alt="" loading="lazy"></img>
+            </div>
+            <div class="tall lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588499756884-d72584d84df5?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2134&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="big lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588492885706-b8917f06df77?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1951&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588247866001-68fa8c438dd7?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=564&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="wide lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1586521995568-39abaa0c2311?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="big lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1572914857229-37bf6ee8101c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1951&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="tall lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588453862014-cd1a9ad06a12?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588414734732-660b07304ddb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=675&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588224575346-501f5880ef29?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+            <div class="lc-block">
+                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1574798834926-b39501d8eda2?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80" alt="" loading="lazy"></img>
+            </div>
+        </div>
+    </div>
+    <Footer />
+    </>
+
   )
 }
