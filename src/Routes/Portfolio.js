92c847a81d450539feaf9be099895f958077cea0
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-07-05 19:36:01 +0200

    added filter

--- src/Routes/Portfolio.js
+++ src/Routes/Portfolio.js
@@ -1,61 +1,84 @@
-import React from 'react';
+import React, { useState } from "react";
+import Menu from "./gallery.js";
+import { Col, Row, Image, Button } from 'react-bootstrap';
 import './portfolio.css'
 import Footer from './Footer.js'
 
-
 export default function Portfolio() {
+  const [items, setItems] = useState(Menu);
+  const [active, setActive] = useState(false);
+  
+  const filterItem = (categItem) => {
+    const updateItems = Menu.filter((curElem) => {
+      return curElem.category === categItem;
+    });
+
+    setItems(updateItems);
+    setActive(true);
+  };
+  
   return (
     <>
         <h1 className='title-page'>PORTFOLIO</h1>
     <hr></hr>
-          <div class="container overflow-hidden py-6">
-        <div class="grid-wrapper col">
-            <div class="lc-block">
-                <img class="img-fluid rounded rounded" src="https://images.unsplash.com/photo-1541845157-a6d2d100c931?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588282322673-c31965a75c3e?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1351&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="tall lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588117472013-59bb13edafec?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=500&amp;q=60" alt="" loading="lazy"></img>
-            </div>
-            <div class="wide lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1587588354456-ae376af71a25?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="lc-block">
-                <img class="img-fluid rounded" src=" https://images.unsplash.com/photo-1558980663-3685c1d673c4?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=60" alt="" loading="lazy"></img>
-            </div>
-            <div class="tall lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588499756884-d72584d84df5?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=2134&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="big lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588492885706-b8917f06df77?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1951&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588247866001-68fa8c438dd7?ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=564&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="wide lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1586521995568-39abaa0c2311?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1350&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="big lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1572914857229-37bf6ee8101c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1951&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="tall lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588453862014-cd1a9ad06a12?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588414734732-660b07304ddb?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=675&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1588224575346-501f5880ef29?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=700&amp;q=80" alt="" loading="lazy"></img>
-            </div>
-            <div class="lc-block">
-                <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1574798834926-b39501d8eda2?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=80" alt="" loading="lazy"></img>
-            </div>
+      <div className="container">
+        <div className="d-flex justify-content-center">
+          <Button variant="outline-light" className={active === "Bébé" ? "active" : ""} onClick={() => filterItem("Bébé")}>
+            Bébé
+          </Button>
+          <Button variant="outline-light" className={active === "Mariage" ? "active" : ""} onClick={() => filterItem("Mariage")}>
+            Mariage
+          </Button>
+          <Button variant="outline-light" className={active === "Grossesse" ? "active" : ""} onClick={() => filterItem("Grossesse")}>
+            Grossesse
+          </Button>
+          <Button variant="outline-light" className={active === "Baptême" ? "active" : ""} onClick={() => filterItem("Baptême")}>
+            Baptême
+          </Button>
+          <Button variant="outline-light" className={active === "Couple" ? "active" : ""} onClick={() => filterItem("Couple")}>
+            Couple
+          </Button>
+          <Button variant="outline-light" className={active === "Famille" ? "active" : ""} onClick={() => filterItem("Famille")}>
+            Famille
+          </Button>
+          <Button variant="outline-light" className={active === "All" ? "active" : ""} onClick={() => setItems(Menu)}>
+            All
+          </Button>
         </div>
-    </div>
-    <Footer />
-    </>
+      </div>
 
+      <div className="container-fluid mt-4">
+        <Row>
+          {items.map((elem) => {
+            const { id, image } = elem;
+
+            return (
+              <Col sm={4} className="d-flex justify-content-center align-items-center" key={id}>
+                <div className="custom-image-container">
+                  <div className="custom-image" style={{ backgroundImage: `url(${image})` }}></div>
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
+
+      <div className="container-fluid mt-4">
+        <Row>
+          {items.map((elem) => {
+            const { id, name, image } = elem;
+
+            return (
+              <Col sm={4} className="d-flex justify-content-center align-items-center" key={id}>
+                <div style={{ margin: '20px' }}>
+                  <Image src={image} alt={name} fluid style={{ marginRight: '20px' }} className="custom-image" />
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </div>
+      <Footer/>
+    </>
   )
 }
