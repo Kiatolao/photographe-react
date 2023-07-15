42a5e45d88577fbed50cd173b658f04e26c7ca7b
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-07-15 16:33:22 +0200

    added contact description in CMS

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -1,47 +1,56 @@
-import React from 'react'
-import '../index.css'
-import Footer from '../components/Footer.js'
-
+import React from 'react';
+import '../index.css';
+import Footer from '../components/Footer.js';
+import useFetch from '../components/useFetch';
 
 export default function Contact() {
+  const { fetchData: detailData, isLoading } = useFetch('http://localhost:8055/items/contact');
+
   return (
     <>
-    <h1 className='title-page'>CONTACT</h1>
-    <hr></hr>
-    <div>
-      <p className='text-contact'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mollis, odio varius venenatis eleifend, libero nibh pellentesque diam, id consequat urna est at ante. Morbi bibendum laoreet viverra. Morbi mattis, ligula sit amet facilisis consectetur, ligula lectus pulvinar nibh, ac lobortis erat justo eu erat. Morbi in lobortis massa, vitae.
-      </p>
-      <div className='text-contact'>
-        <p>Charles Cantin <br></br>
-        17 rue Nowhere<br></br>
-        Nonecity 77777<br></br>
-        02 03 04 05 06</p>
-      </div>
-      <hr></hr>
-      <div className='form-position'>
-        <form name="contact" method="POST" data-netlify="true">
-          <p>
-          <label>Prénom: <input type="text" name="surname" /></label>
-          </p>
-          <p>
-            <label>Nom: <input type="text" name="name" /></label>
-          </p>
-          <p>
-            <label>Email: <input type="email" name="email" /></label>
-          </p>
-          <p>
-          <label>Objet: <input type="text" name="object" /></label>
-          </p>
-          <p>
-            <label>Message: <textarea name="message"></textarea></label>
-          </p>
-          <p>
-            <button type="submit" className='button-87'>Envoyer</button>
-          </p>
-        </form>
-      </div>
-    </div>
-    <Footer />
+      <h1 className='title-page'>CONTACT</h1>
+      <hr />
+      {isLoading ? (
+        <p>Chargement...</p>
+      ) : (
+        detailData && (
+          <div key={detailData.id}>
+            <p className='text-contact'>{detailData.description}</p>
+            <div className='text-contact'>
+              <p>
+                {detailData.nom} <br />
+                {detailData.adresse}<br />
+                {detailData.ville}<br />
+                {detailData.telephone}
+              </p>
+            </div>
+            <hr />
+            <div className='form-position'>
+              <form name="contact" method="POST" data-netlify="true">
+                <p>
+                  <label>Prénom: <input type="text" name="surname" /></label>
+                </p>
+                <p>
+                  <label>Nom: <input type="text" name="name" /></label>
+                </p>
+                <p>
+                  <label>Email: <input type="email" name="email" /></label>
+                </p>
+                <p>
+                  <label>Objet: <input type="text" name="object" /></label>
+                </p>
+                <p>
+                  <label>Message: <textarea name="message"></textarea></label>
+                </p>
+                <p>
+                  <button type="submit" className='button-87'>Envoyer</button>
+                </p>
+              </form>
+            </div>
+          </div>
+        )
+      )}
+      <Footer />
     </>
-  )
+  );
 }
