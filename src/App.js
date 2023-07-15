573bbd1dd9c1ffbe12473e4ffd71f0d8971e9799
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-07-15 21:20:36 +0200

    reworked portofolio with CMS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './index.css';
 import React from 'react';
 import Header from './components/Header';
-
 import { Outlet } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
--- src/Routes/Portfolio.js
+++ src/Routes/Portfolio.js
@@ -1,95 +1,49 @@
-import React, { useState } from "react";
-import Menu from "./gallery.js";
-import { Col, Row, Image, Button } from 'react-bootstrap';
+import React from "react";
+import { Row, Col, Image, Button } from 'react-bootstrap';
 import '../index.css';
 import Footer from '../components/Footer.js'
+import useFetch from '../components/useFetch';
 
 export default function Portfolio() {
-  const [items, setItems] = useState(Menu);
-  const [active, setActive] = useState(false);
-  const [selectedImage, setSelectedImage] = useState(null);
+  const { fetchData: buttonData, isLoading } = useFetch('http://localhost:8055/items/bouton');
+  const { fetchData: galleryData} = useFetch('http://localhost:8055/items/gallerie');
+  console.log(galleryData);
 
-  const filterItem = (categItem) => {
-    const updateItems = Menu.filter((curElem) => {
-      return curElem.category === categItem;
-    });
-
-    setItems(updateItems);
-    setActive(true);
-  };
-
-  const openPopup = (image) => {
-    setSelectedImage(image);
-  };
-
-  const closePopup = () => {
-    setSelectedImage(null);
-  };
-  
   return (
     <>
       <h1 className='title-page'>PORTFOLIO</h1>
       <hr />
-      <div className="container">
-  <div className="d-flex flex-wrap justify-content-center button-style">
-    <Button variant="outline-light" className={active === "Bébé" ? "active" : ""} onClick={() => filterItem("Bébé")} style={{margin: "5px"}}>
-      Bébé
-    </Button>
-    <Button variant="outline-light" className={active === "Mariage" ? "active" : ""} onClick={() => filterItem("Mariage")} style={{margin: "5px"}}>
-      Mariage
-    </Button>
-    <Button variant="outline-light" className={active === "Grossesse" ? "active" : ""} onClick={() => filterItem("Grossesse")} style={{margin: "5px"}}>
-      Grossesse
-    </Button>
-    <Button variant="outline-light" className={active === "Baptême" ? "active" : ""} onClick={() => filterItem("Baptême")} style={{margin: "5px"}}>
-      Baptême
-    </Button>
-    <Button variant="outline-light" className={active === "Couple" ? "active" : ""} onClick={() => filterItem("Couple")} style={{margin: "5px"}}>
-      Couple
-    </Button>
-    <Button variant="outline-light" className={active === "Famille" ? "active" : ""} onClick={() => filterItem("Famille")} style={{margin: "5px"}}>
-      Famille
-    </Button>
-    <Button variant="outline-light" className={active === "All" ? "active" : ""} onClick={() => setItems(Menu)} style={{margin: "5px"}}>
-      Toutes
-    </Button>
-  </div>
-</div>
-
-      <div className="container-fluid mt-4 ">
-        <Row>
-          {items.map((elem) => {
-            const { id, name, image } = elem;
-
-            return (
-              <Col sm={4} className="d-flex justify-content-center align-items-center" key={id}>
-                <div style={{ marginBottom: '15px' }}>
-                  <Image
-                    src={image}
-                    alt={name}
-                    fluid
-                    className="image-container"
-                    onClick={() => openPopup(image)}
-                  />
-                </div>
-              </Col>
-            );
-          })}
-        </Row>
-      </div>
-
-      {selectedImage && (
-        <div className="popup">
-          <div className="popup-content">
-            <Image src={selectedImage} alt="Popup" fluid  className="pop-container"/>
-            <button className="close-button" onClick={closePopup}>
-               X 
-            </button>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="container">
+          <div className="d-flex flex-wrap justify-content-center button-style">
+            {buttonData.map((button) => (
+              <Button key={button.id} variant="outline-light" style={{ margin: "5px" }}>
+                {button.title}
+              </Button>
+            ))}
           </div>
         </div>
       )}
 
-      <Footer />
+<div className="container-fluid mt-4">
+  <Row>
+    {galleryData && galleryData.map((gallery) => (
+      <Col sm={4} className="d-flex justify-content-center align-items-center" key={gallery.id}>
+        <div style={{ marginBottom: '15px' }}>
+          <Image
+            src={`http://localhost:8055/assets/${gallery.image}`}
+            alt= {gallery.title}
+            fluid
+            className="image-container"
+          />
+        </div>
+      </Col>
+    ))}
+  </Row>
+</div>
+  <Footer />
     </>
   );
 }
