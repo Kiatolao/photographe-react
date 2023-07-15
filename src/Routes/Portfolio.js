745a1ea3c59c3be9c62d44b8a097133b6f052f92
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-07-16 01:07:54 +0200

    upadated portfolio

--- src/Routes/Portfolio.js
+++ src/Routes/Portfolio.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col, Image, Button } from 'react-bootstrap';
 import '../index.css';
 import Footer from '../components/Footer.js'
 import useFetch from '../components/useFetch';
 
 export default function Portfolio() {
+  const [selectedFilter, setSelectedFilter] = useState(''); 
   const { fetchData: buttonData, isLoading } = useFetch('http://localhost:8055/items/bouton');
-  const { fetchData: galleryData} = useFetch('http://localhost:8055/items/gallerie');
+  const { fetchData: galleryData } = useFetch('http://localhost:8055/items/gallerie');
   console.log(galleryData);
 
   return (
@@ -19,31 +20,46 @@ export default function Portfolio() {
         <div className="container">
           <div className="d-flex flex-wrap justify-content-center button-style">
             {buttonData.map((button) => (
-              <Button key={button.id} variant="outline-light" style={{ margin: "5px" }}>
+              <Button
+                key={button.id}
+                variant="outline-light"
+                style={{ margin: "5px" }}
+                onClick={() => setSelectedFilter(button.title)} 
+              >
                 {button.title}
               </Button>
             ))}
+            <Button
+              variant="outline-light"
+              style={{ margin: "5px" }}
+              onClick={() => setSelectedFilter('')} 
+            >
+              Toutes
+            </Button>
           </div>
         </div>
       )}
 
-<div className="container-fluid mt-4">
-  <Row>
-    {galleryData && galleryData.map((gallery) => (
-      <Col sm={4} className="d-flex justify-content-center align-items-center" key={gallery.id}>
-        <div style={{ marginBottom: '15px' }}>
-          <Image
-            src={`http://localhost:8055/assets/${gallery.image}`}
-            alt= {gallery.title}
-            fluid
-            className="image-container"
-          />
-        </div>
-      </Col>
-    ))}
-  </Row>
-</div>
-  <Footer />
+      <div className="container-fluid mt-4">
+        <Row>
+          {galleryData &&
+            galleryData
+            .filter((gallery) => selectedFilter === '' || gallery.tag == selectedFilter) 
+              .map((gallery) => (
+                <Col sm={4} className="d-flex justify-content-center align-items-center" key={gallery.id}>
+                  <div style={{ marginBottom: '15px' }}>
+                    <Image
+                      src={`http://localhost:8055/assets/${gallery.image}`}
+                      alt={gallery.title}
+                      fluid
+                      className={`image-container ${gallery.tag}`}
+                    />
+                  </div>
+                </Col>
+              ))}
+        </Row>
+      </div>
+      <Footer />
     </>
   );
 }
