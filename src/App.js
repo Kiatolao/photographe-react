19fddf84416d081802938ef521b996a650e43df6
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-07-17 22:10:31 +0200

    added browerrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './index.css';
-import React from 'react';
+import { React, BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import { Outlet } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,9 +8,12 @@ import Layer from './components/layer.js';
 function App() {
   return (
     <>
+    <BrowserRouter>
       <Layer />
       <Header />
       <Outlet />
+    </BrowserRouter>
+
     </>
   );
 }
