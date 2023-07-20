c413dede8cc57dfb607f9f2b6370983d6c56993d
Kiatolao <94093817+Kiatolao@users.noreply.github.com>
2023-07-20 20:07:37 +0200

    added title custom

--- src/Routes/Accueil.js
+++ src/Routes/Accueil.js
@@ -3,10 +3,11 @@ import '../index.css';
 import useFetch from '../components/useFetch';
 import Loading from '../components/Loading';
 
+
 export default function Accueil() {
   // Récupération de l'API
   const { fetchData, isLoading } = useFetch('https://directus-production-36e3.up.railway.app/items/background');
-
+  const { fetchData : titleData } = useFetch('https://directus-production-36e3.up.railway.app/items/titre');
   return (
     <>
       <div className="fullscreen-bg"
@@ -20,12 +21,14 @@ export default function Accueil() {
           <img src={`https://directus-production-36e3.up.railway.app/assets/${fetchData.image}`} alt={fetchData.title} className="charles" />
         )
       )}
-
+      {titleData && (
       <div className="centered-text">
-        <h1 className='title-splash'>Charles Cantin</h1>
-        <h1 className='title-splash'>Photographe</h1>
+        <h1 className='title-splash'>{titleData.nom}</h1>
+        <h1 className='title-splash'>{titleData.profession}</h1>
       </div>
+      )}
     </div>
+
     </>
   );
 }
